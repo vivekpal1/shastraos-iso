196194e335f25fc092bde9e0796d44d24b62fbdd
vivek <31vivekpal@gmail.com>
2022-04-14 22:47:14 +0530

    extesions update

---
+++ etc/skel/.local/share/gnome-shell/extensions/logomenu@aryan_k/constants.js
@@ -0,0 +1,7 @@
+var DistroIcons = [
+    { PATH: 'start-here-symbolic'},
+    { PATH: '/Resources/shastra-logo.svg'},
+    { PATH: '/Resources/tux-logo-symbolic.svg'},
+    { PATH: '/Resources/arch-logo.svg'},
+    { PATH: '/Resources/tux-logo.svg'},
+]
