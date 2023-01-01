72b5c8bdf67fd3d6fa33e9158ec5bda2ea1872c8
vivekpal1 <vivekpal@duck.com>
2023-01-01 20:36:22 +0530

    Wallpapers

--- etc/skel/.local/share/gnome-shell/extensions/gestureImprovements@gestures/src/utils/environment.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* exported easeActor, easeAdjustment */
-function easeActor(actor, params) {
-	actor.ease(params);
-}
-
-function easeAdjustment(actor, value, params) {
-	actor.ease(value, params);
-}
